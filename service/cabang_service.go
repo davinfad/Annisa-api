31d0ccb16cc74badb5751c4ef43a0f2fea4c46e9
raihan2-byte <raihanalfarisi4@gmail.com>
2025-04-30 22:48:42 +0700

    fix bugs

--- service/cabang_service.go
+++ service/cabang_service.go
@@ -9,7 +9,6 @@ import (
 
 type ServiceCabang interface {
 	Create(cabang *models.CabangDTO) (*models.Cabang, error)
-	GetByID(id int) (*models.Cabang, error)
 }
 
 type serviceCabang struct {
@@ -18,17 +17,6 @@ type serviceCabang struct {
 
 func NewCabangService(repositoryCabang repository.RepositoryCabang) *serviceCabang {
 	return &serviceCabang{repositoryCabang}
-}
-
-func (s *serviceCabang) GetByID(id int) (*models.Cabang, error) {
-	cabang, err := s.repositoryCabang.GetByID(id)
-	if err != nil {
-		return nil, err
-	}
-	if cabang == nil {
-		return nil, err
-	}
-	return cabang, nil
 }
 
 func (s *serviceCabang) Create(cabang *models.CabangDTO) (*models.Cabang, error) {
