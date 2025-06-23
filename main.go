037d720d7445178f40dc4742ae71938a4bdd54f3
davinfad <davin.fad@student.ub.ac.id>
2025-06-24 00:35:37 +0700

    [Dev] Fix main

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"annisa-api/handler"
+
+	"github.com/joho/godotenv"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	// Load environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Start the app
 	handler.StartApp()
 }
